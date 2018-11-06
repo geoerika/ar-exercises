32f0c492353b94c46de8b37a016fe4bd718f352e
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-06 19:30:34 +0000

    solved exercise 7, validation for Store and Employee model

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,32 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+print "Which store do you go shopping to? "
+@store_name = gets.chomp
+@store7 = Store.create!(name: "#{@store_name}")
+@store7.save
+
+@store7.errors.full_messages.each do |message|
+  puts message
+end
+
+puts Store.count
+
+# ActiveRecord::Schema.define do
+#   drop_table :stores if ActiveRecord::Base.connection.table_exists?(:stores)
+#   drop_table :employees if ActiveRecord::Base.connection.table_exists?(:employees)
+#   create_table :stores do |t|
+#     t.column :name, :string
+#     t.column :annual_revenue, :integer
+#     t.column :mens_apparel, :boolean
+#     t.column :womens_apparel, :boolean
+#     t.timestamps null: false
+#   end
+#   create_table :employees do |table|
+#     table.references :store
+#     table.column :first_name, :string
+#     table.column :last_name, :string
+#     table.column :hourly_rate, :integer
+#     table.timestamps null: false
+#   end
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,25 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200}
+  validates :store_id, presence: true
 end
+
+# ActiveRecord::Schema.define do
+#   drop_table :stores if ActiveRecord::Base.connection.table_exists?(:stores)
+#   drop_table :employees if ActiveRecord::Base.connection.table_exists?(:employees)
+#   create_table :stores do |t|
+#     t.column :name, :string
+#     t.column :annual_revenue, :integer
+#     t.column :mens_apparel, :boolean
+#     t.column :womens_apparel, :boolean
+#     t.timestamps null: false
+#   end
+#   create_table :employees do |table|
+#     table.references :store
+#     table.column :first_name, :string
+#     table.column :last_name, :string
+#     table.column :hourly_rate, :integer
+#     table.timestamps null: false
+#   end
