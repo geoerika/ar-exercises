5f752d8d5814856cd53d373e52b8c3346244ecb8
Erika Szabo <erika.szabo.geo@gmail.com>
2018-11-06 18:06:00 +0000

    solved exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store3 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store3.save
+store4 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store4.save
+store5 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+store5.save
+
+@men_stores = Store.where(mens_apparel: true).order(id: :asc)
+@men_stores.each do |store|
+  puts store.name
+end
+
+
+puts Store.count
